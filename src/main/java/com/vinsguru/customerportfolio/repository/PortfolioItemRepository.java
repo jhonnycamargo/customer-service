e2826625155f700376dee0a826a7ef6651a03e56
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2024-10-25 19:44:26 -0400

    Find by customer id and ticker.

--- src/main/java/com/vinsguru/customerportfolio/repository/PortfolioItemRepository.java
+++ src/main/java/com/vinsguru/customerportfolio/repository/PortfolioItemRepository.java
@@ -4,9 +4,12 @@ import com.vinsguru.customerportfolio.entity.PortfolioItem;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface PortfolioItemRepository extends ReactiveCrudRepository<PortfolioItem, Integer> {
 
     Flux<PortfolioItem> findAllByCustomerId(Integer customerId);
+
+    Mono<PortfolioItem> findByCustomerIdAndTicker(Integer customerId, String ticker);
 }
