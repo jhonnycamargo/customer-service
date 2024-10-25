a03e03ad9a51dce5c0e608c63c48af4182e23216
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2024-10-25 19:35:20 -0400

    Trade actions.

--- src/main/java/com/vinsguru/customerportfolio/domain/TradeAction.java
+++ src/main/java/com/vinsguru/customerportfolio/domain/TradeAction.java
@@ -1,4 +1,6 @@
 package com.vinsguru.customerportfolio.domain;
 
 public enum TradeAction {
+    BUY,
+    SELL
 }
---
+++ src/main/java/com/vinsguru/customerportfolio/service/TradeService.java
@@ -0,0 +1,36 @@
+package com.vinsguru.customerportfolio.service;
+
+import com.vinsguru.customerportfolio.dto.StockTradeRequest;
+import com.vinsguru.customerportfolio.dto.StockTradeResponse;
+import com.vinsguru.customerportfolio.repository.CustomerRepository;
+import com.vinsguru.customerportfolio.repository.PortfolioItemRepository;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class TradeService {
+
+    private final CustomerRepository customerRepository;
+    private final PortfolioItemRepository portfolioItemRepository;
+
+    public TradeService(CustomerRepository customerRepository, PortfolioItemRepository portfolioItemRepository) {
+        this.customerRepository = customerRepository;
+        this.portfolioItemRepository = portfolioItemRepository;
+    }
+
+    public Mono<StockTradeResponse> trade(Integer customerId, StockTradeRequest request){
+        return switch (request.tradeAction()) {
+            case BUY -> buyStock(customerId, request);
+            case SELL -> sellStock(customerId, request);
+        };
+    }
+
+    private Mono<StockTradeResponse> buyStock(Integer customerId, StockTradeRequest request){
+        return Mono.empty();
+    }
+
+    private Mono<StockTradeResponse> sellStock(Integer customerId, StockTradeRequest request){
+        return Mono.empty();
+    }
+
+}
