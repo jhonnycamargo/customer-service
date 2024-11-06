4da7ef46c5fd861bf4b49128621f107f2e0b5e00
Jhonny I. Camargo O. <jhonnycamargo@gmail.com>
2024-11-05 20:14:01 -0500

    GenericException second commit.

--- src/main/java/com/vinsguru/customerportfolio/exceptions/GenericException.java
+++ src/main/java/com/vinsguru/customerportfolio/exceptions/GenericException.java
@@ -1,4 +1,10 @@
 package com.vinsguru.customerportfolio.exceptions;
 
-public class GenericException {
+public class GenericException extends RuntimeException {
+
+    private static final String MESSAGE = "Error occurred for customer [id=%d]";
+
+    public GenericException(Integer id) {
+        super(MESSAGE.formatted(id));
+    }
 }
